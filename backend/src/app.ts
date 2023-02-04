f7f01e05820515b50cd3e8ba037f320fe2c3a5ae
wendelbueres <wendelbueres@gmail.com>
2023-02-03 23:41:37 -0300

    feat: :sparkles: adicionado cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,22 @@
-// import "reflect-metadata";
+import cors from "cors";
 import express from "express";
 import { handleErrorMidleware } from "./middlewares/handleError.midleware";
 import "express-async-errors";
 import userRoutes from "./routes/user.router";
 import contactEmailRoutes from "./routes/contactEmail.router";
 import contactPhoneRoutes from "./routes/contactPhone.router";
-// import swaggerUi from "swagger-ui-express";
-// import swaggerDocs from "./swagger.json";
 const app = express();
 
-app.use(express.json());
-// app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use("/users", userRoutes);
-app.use("/email", contactEmailRoutes);
-app.use("/phone", contactPhoneRoutes);
-app.use(handleErrorMidleware);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  app.use(cors());
+  app.use(express.json());
+  app.use("/users", userRoutes);
+  app.use("/email", contactEmailRoutes);
+  app.use("/phone", contactPhoneRoutes);
+  app.use(handleErrorMidleware);
+  next();
+});
 
 export default app;
